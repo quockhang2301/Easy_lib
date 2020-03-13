01837a78c4f15732889acab69603f0781efe91d1
quockhang2301 <56494567+quockhang2301@users.noreply.github.com>
2020-03-13 14:58:13 +0700

    change 3

--- app/src/main/java/com/quockhang/easy_learning/MainActivity.java
+++ app/src/main/java/com/quockhang/easy_learning/MainActivity.java
@@ -1,15 +1,20 @@
 package com.quockhang.easy_learning;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.quockhang.easy_lib.helpers.Simple_help;
+
 public class MainActivity extends AppCompatActivity {
+    
+    public static final int hell = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.w("easy", Simple_help.hello());
     }
 }
