2b969aea1da0542f079c436f74475b28031cec89
quockhang2301 <trinhquockhang2301@gmail.com>
2020-03-13 18:16:41 +0700

    change

--- app/src/main/java/com/quockhang/easy_learning/MainActivity.java
+++ app/src/main/java/com/quockhang/easy_learning/MainActivity.java
@@ -9,12 +9,13 @@ import com.quockhang.easy_lib.helpers.Simple_help;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int hell = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.w("easy", Simple_help.hello());
+
     }
 }
